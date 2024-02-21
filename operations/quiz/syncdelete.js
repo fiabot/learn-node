3d5aa848d437fa9f4463fec7cddcb954efecb167
Fiona Shyne <shynef@union.edu>
2024-02-21 16:21:28 -0500

    updated operations

--- operations/quiz/syncdelete.js
+++ operations/quiz/syncdelete.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
-fs.readFileSync("./operations/data.txt", (err, data) => {
+fs.readFile("./operations/data.txt", (err, data) => {
   if (err) console.log(err.message);
-  else console.log(data);
+  else {
+    console.log(data);
+    fs.unlinkSync("./operations/data.txt");		// delete the file
+  }
 });
-fs.unlinkSync("./operations/data.txt");		// delete the file
+
 
